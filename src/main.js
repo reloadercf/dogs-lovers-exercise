7d7358f28b58db1536332223330a3b83b23e1603
reloadercf <elcfm1@gmail.com>
2022-03-04 09:29:51 -0600

    delete

--- src/main.js
+++ src/main.js
@@ -1,12 +1,2 @@
 import data from './data.js'
 import {generadorHtml} from './generadorHtml.js'
-
-const nodoRoot=document.getElementById('root')
-
-let todoElHTML=""
-
-data.dogs.forEach(perrito=>{
-    todoElHTML+=generadorHtml(perrito)
-})
-
-nodoRoot.innerHTML=todoElHTML
